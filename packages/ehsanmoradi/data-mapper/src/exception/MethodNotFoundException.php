dfe73fec647480b9f3daad6087334fac59604cbe
ehsan moradi <ehsanmoradi.a@gmail.com>
2023-08-08 09:12:02 +0330

    Update MethodNotFoundException.php

--- packages/ehsanmoradi/data-mapper/src/exception/MethodNotFoundException.php
+++ packages/ehsanmoradi/data-mapper/src/exception/MethodNotFoundException.php
@@ -7,8 +7,5 @@ use Throwable;
 
 class MethodNotFoundException extends Exception
 {
-    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
+
 }
